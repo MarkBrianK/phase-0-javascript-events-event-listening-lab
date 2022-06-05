4c97b679da438460dbeb1be3afb1d86084ee2131
MarkBrianKariuki <markbrian.karanja@student.moringaschoool.com>
2022-06-05 19:32:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+function addingEventListener(){
+    alert ('I was clicked!');
+    input.addEventListener('click', addingEventListener);
 }
